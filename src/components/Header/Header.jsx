525de67b87d5a6fa87db7dbb5ddcd22feaf0cf34
Emmanuel Guefif <eguefif@fastmail.com>
2025-07-12 20:43:51 -0400

    Ex2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import Icon from '../Icon';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -16,29 +17,69 @@ const Header = () => {
 
   return (
     <header>
-      <SuperHeader />
-      <MainHeader>
-        <Side>
-          <Logo />
-        </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
-      </MainHeader>
+      <Wrapper>
+          <BigHeaderWrapper>
+              <SuperHeader />
+              <MainHeader>
+                <Side>
+                  <Logo />
+                </Side>
+                <Nav>
+                  <NavLink href="/sale">Sale</NavLink>
+                  <NavLink href="/new">New&nbsp;Releases</NavLink>
+                  <NavLink href="/men">Men</NavLink>
+                  <NavLink href="/women">Women</NavLink>
+                  <NavLink href="/kids">Kids</NavLink>
+                  <NavLink href="/collections">Collections</NavLink>
+                </Nav>
+                <Side />
+              </MainHeader>
+          </BigHeaderWrapper>
 
-      <MobileMenu
-        isOpen={showMobileMenu}
-        onDismiss={() => setShowMobileMenu(false)}
-      />
+          <MobileWrapper>
+              <LogoWrapper>
+                <Logo />
+              </LogoWrapper>
+              <Icon id='shopping-bag' />
+              <Icon id='search' />
+              <Icon id='menu' />
+              <MobileMenu
+                isOpen={showMobileMenu}
+                onDismiss={() => setShowMobileMenu(false)}
+              />
+          </MobileWrapper>
+      </Wrapper>
     </header>
   );
 };
+
+const Wrapper = styled.div`
+    border-bottom: 1px solid ${COLORS.gray[300]};
+    margin: 0;
+    padding: 0;
+`;
+
+const LogoWrapper = styled.div`
+    margin-right: auto;
+`;
+
+const BigHeaderWrapper = styled.div`
+    @media (max-width: ${QUERIES.tabletAndDown}) {
+        display: none;
+    }
+`;
+
+
+const MobileWrapper = styled.div`
+    display: none;
+    padding: 24px 29px 24px 32px;
+
+    @media (max-width: ${QUERIES.tabletAndDown}) {
+        display: flex;
+        gap: 34px;
+        padding
+    }
+`;
 
 const MainHeader = styled.div`
   display: flex;
@@ -46,6 +87,10 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+    @media (max-width: ${QUERIES.tabletAndDown}) {
+        display: none;
+    }
 `;
 
 const Nav = styled.nav`
